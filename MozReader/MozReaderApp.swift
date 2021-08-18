557793a5ee224ad8f59ee8a67eefd57c9a69ac72
Khanh <khanh.tbh@gmail.com>
2021-08-18 21:47:15 +0700

    Initial Commit

---
+++ MozReader/MozReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MozReaderApp.swift
+//  MozReader
+//
+//  Created by Khanh Trinh on 18/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct MozReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
